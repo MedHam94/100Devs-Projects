f564bb3a5e5eda4ad5f0d70893231d938403dd1c
MedHammas <hammas.m.mh@gmail.com>
2023-07-05 15:04:46 +0100

    randomize the image selection

--- api-practice/js/main.js
+++ api-practice/js/main.js
@@ -1,20 +1,26 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFact)
+document.querySelector('button').addEventListener('click', getImage)
 
 
 function getFact(){
   const factUrl = 'https://catfact.ninja/fact'
-  const imgUrl= 'https://cataas.com/api/cats';
   fetch(factUrl)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         document.querySelector('.facts').innerHTML = data.fact
       })
+  
+}
+
+function getImage(){
+  const imgUrl= 'https://cataas.com/api/cats';
   fetch(imgUrl)
       .then(res=> res.json())
       .then(data=> {
-        document.querySelector('.randomImg').src = `https://cataas.com/cat/${data._id}`
+        
+        let index = Math.floor(Math.random() * data.length)
+        document.querySelector('.randomImg').src = `https://cataas.com/cat/${data[index]._id}`
       })
 }
-
 
